e01fd907dbcba261963c6932d68d8015c1244dda
youngyounkim <rladuddbs789@gmail.com>
2023-11-28 15:41:51 +0900

    feat: cube mesh 추가

--- 1-cube/src/main.js
+++ 1-cube/src/main.js
@@ -1,7 +1,11 @@
 import * as THREE from "three";
 
 const init = () => {
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
   document.body.appendChild(renderer.domElement);
 
@@ -13,6 +17,17 @@ const init = () => {
     1,
     500
   );
+
+  const geometry = new THREE.BoxGeometry(2, 2, 2);
+  const material = new THREE.MeshBasicMaterial({ conlor: 0xcc99ff });
+
+  const cube = new THREE.Mesh(geometry, material);
+
+  sceen.add(cube);
+
+  camera.position.set(3, 4, 5);
+
+  camera.lookAt(cube.position);
 
   renderer.render(sceen, camera);
 };
