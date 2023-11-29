e82ea54c6b23b8bdd38a12193c68357a3adf5512
youngyounkim <rladuddbs789@gmail.com>
2023-11-29 13:50:14 +0900

    feat: 템플릿 수정

--- 0-template/src/main.js
+++ 0-template/src/main.js
@@ -1,7 +1,51 @@
 import * as THREE from "three";
 
 const init = () => {
-  console.log(THREE);
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  const sceen = new THREE.Scene();
+  // 시야각,  ,얼마나 가까이, 얼마나 멀리
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+
+  camera.position.z = 5;
+
+  const render = () => {
+    const elapsedTime = clock.getElapsedTime();
+
+    // cube.rotation.x = elapsedTime; // x 축 방향으로 돌리는 것 단위는 라디안
+    // cube.rotation.y = elapsedTime;
+
+    // skeleton.rotation.x = elapsedTime * 1.5;
+    // skeleton.rotation.y = elapsedTime * 1.5;
+    // cube.position.y = Math.sin(cube.rotation.x);
+    // cube.scale.x = Math.cos(cube.rotation.x);
+    controls.update();
+    renderer.render(sceen, camera);
+    requestAnimationFrame(render);
+  };
+
+  render();
+
+  const handleResize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    controls.update();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(sceen, camera);
+  };
+
+  window.addEventListener("resize", handleResize);
 };
 
 window.addEventListener("load", () => {
