250e7838f3da8b02d2f6b5918e276ea92ececaa9
youngyounkim <rladuddbs789@gmail.com>
2023-12-01 15:26:16 +0900

    feat: 폰트 랜더링 코드 추가

--- 2-text/src/main.js
+++ 2-text/src/main.js
@@ -1,8 +1,12 @@
 import * as THREE from "three";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
-import typeface from "./assets/fonts/The Jamsil 3 Regular_Regular.json";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import GUI from "lil-gui";
 
 const init = () => {
+  const gui = new GUI();
+
   const renderer = new THREE.WebGLRenderer({
     antialias: true,
   });
@@ -11,7 +15,7 @@ const init = () => {
 
   document.body.appendChild(renderer.domElement);
 
-  const sceen = new THREE.Scene();
+  const scene = new THREE.Scene();
   // 시야각,  ,얼마나 가까이, 얼마나 멀리
   const camera = new THREE.PerspectiveCamera(
     75,
@@ -22,26 +26,46 @@ const init = () => {
 
   camera.position.z = 5;
 
+  new OrbitControls(camera, renderer.domElement);
+
   // Font
   const fontLoader = new FontLoader();
 
-  // fontLoader.load(
-  //   "./assets/fonts/The Jamsil 3 Regular_Regular.json",
-  //   (font) => {
-  //     console.log(font);
-  //   },
-  //   (event) => {
-  //     console.log(event);
-  //   },
-  //   (error) => {
-  //     console.log(error);
-  //   }
-  // );
+  let textGeometry, textMaterial;
 
-  const font = fontLoader.parse(typeface);
+  fontLoader.load(
+    "./assets/fonts/The Jamsil 3 Regular_Regular.json",
+    (font) => {
+      textGeometry = new TextGeometry("안녕하세요", {
+        font,
+        size: 0.5,
+        height: 0.1,
+      });
+
+      textMaterial = new THREE.MeshPhongMaterial({ color: 0x00c896 });
+
+      const text = new THREE.Mesh(textGeometry, textMaterial);
+
+      scene.add(text);
+    }
+  );
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+
+  scene.add(ambientLight);
+  // const font = fontLoader.parse(typeface);
+
+  const pointLight = new THREE.PointLight(0xffffff, 0.5);
+  const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.5);
+
+  pointLight.position.set(3, 0, 2);
+
+  scene.add(pointLight, pointLightHelper);
+
+  gui.add(pointLight.position, "x").min(-3).max(3).step(0.1);
 
   const render = () => {
-    renderer.render(sceen, camera);
+    renderer.render(scene, camera);
     requestAnimationFrame(render);
   };
 
@@ -52,7 +76,7 @@ const init = () => {
     camera.updateProjectionMatrix();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.render(sceen, camera);
+    renderer.render(scene, camera);
   };
 
   window.addEventListener("resize", handleResize);
