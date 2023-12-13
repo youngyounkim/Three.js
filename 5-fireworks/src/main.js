7a82c25fca0af9b936b90ee26af37a074fd8dc44
youngyounkim <rladuddbs789@gmail.com>
2023-12-13 14:51:50 +0900

    feat: 프로젝트 생성 및 points 메쉬 추가

---
+++ 5-fireworks/src/main.js
@@ -0,0 +1,56 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const init = () => {
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  const scene = new THREE.Scene();
+  // 시야각,  ,얼마나 가까이, 얼마나 멀리
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+
+  camera.position.z = 5;
+
+  new OrbitControls(camera, renderer.domElement);
+
+  const geometry = new THREE.SphereGeometry();
+  const material = new THREE.PointsMaterial({
+    color: 0xccaaff,
+    size: 0.01,
+  });
+
+  const points = new THREE.Points(geometry, material);
+
+  scene.add(points);
+
+  const render = () => {
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  };
+
+  render();
+
+  const handleResize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  };
+
+  window.addEventListener("resize", handleResize);
+};
+
+window.addEventListener("load", () => {
+  init();
+});
